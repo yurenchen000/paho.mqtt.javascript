c06d66e2cf0d017cc0afe70317ecfdd13e6e94e7
yurenchen <yurenchen@yeah.net>
2019-01-10 23:55:59 +0800

    U utility.js: use simple number client_id

--- utility/utility.js
+++ utility/utility.js
@@ -286,9 +286,10 @@ function safeTagsRegex(str) {
 
 function makeid() {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  // var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var possible = "0123456789";
 
-  for (var i = 0; i < 5; i++)
+  for (var i = 0; i < 4; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
